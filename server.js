3831d71eb5d01c8d83b51a87e9f153ba5b906132
William White, Jr <wewjr82@gmail.com>
2024-04-10 15:17:57 -0400

    uploading church app

--- server.js
+++ server.js
@@ -23,66 +23,18 @@ app.use(express.json());
 // Test route to render a simple EJS template
 // Routes
 app.get("/", (req, res) => {
-  pool
-    .query("SELECT * FROM your_table")
-    .then((result) => {
-      res.render("index", { items: result.rows, title: "CRUD App" });
-    })
-    .catch((error) => res.status(500).send("Internal Server Error"));
+  res.render("index", { title: "Zion Community Church" });
 });
 
-app.post("/create", (req, res) => {
-  const { name, description } = req.body;
+app.post("/signup", (req, res) => {
+  const { first_name, last_name, email, phone_number } = req.body;
   pool
-    .query("INSERT INTO your_table (name, description) VALUES ($1, $2)", [
-      name,
-      description,
-    ])
+    .query(
+      "INSERT INTO members (first_name, last_name, email, phone_number ) VALUES ($1, $2, $3, $4)",
+      [first_name, last_name, email, phone_number]
+    )
     .then(() => {
-      res.redirect("/");
-    })
-    .catch((error) => {
-      console.error("Error executing query", error);
-      res.status(500).send("Internal Server Error");
-    });
-});
-
-app.get("/edit/:id", (req, res) => {
-  const id = req.params.id;
-  pool
-    .query("SELECT * FROM your_table WHERE id = $1", [id])
-    .then((result) => {
-      res.render("edit", { item: result.rows[0] });
-    })
-    .catch((error) => {
-      console.error("Error executing query", error);
-      res.status(500).send("Internal Server Error");
-    });
-});
-
-app.post("/update/:id", (req, res) => {
-  const id = req.params.id;
-  const { name, description } = req.body;
-  pool
-    .query("UPDATE your_table SET name = $1, description = $2 WHERE id = $3", [
-      name,
-      description,
-      id,
-    ])
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((error) => {
-      console.error("Error executing query", error);
-      res.status(500).send("Internal Server Error");
-    });
-});
-
-app.post("/delete/:id", (req, res) => {
-  const id = req.params.id;
-  pool
-    .query("DELETE FROM your_table WHERE id = $1", [id])
-    .then(() => {
+      console.log("Info has been added:", first_name, last_name);
       res.redirect("/");
     })
     .catch((error) => {
