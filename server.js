7b49e282ed7d1a26deaf491899fc55a546f7982f
William White, Jr <wewjr82@gmail.com>
2024-04-13 19:54:19 -0400

    uodate

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ require("dotenv").config();
 const app = express();
 const port = 3000;
 
+const cport = parseInt(process.env.DB_PORT, 10);
+
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: cport,
 });
 
 app.set("view engine", "ejs");
