b6735db4615edef0af044b2998c75c0e5776bcc4
William White, Jr <wewjr82@gmail.com>
2024-04-22 23:17:46 -0400

    removed some info

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ const app = express();
 //   port: cport,
 // });
 
-// console.log(process.env.DATABASE_URL);
-
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -32,7 +30,7 @@ app.get("/visitors", (req, res) => {
   res.render("visitors");
 });
 
-app.post("/signup", (req, res, next) => {
+app.post("/signup", (req, res) => {
   const {
     first_name,
     last_name,
@@ -41,42 +39,49 @@ app.post("/signup", (req, res, next) => {
     date_joined,
     title,
   } = req.body;
-  pool.query(
-    "INSERT INTO members (first_name, last_name, date_of_birth, phone_number, date_joined, title ) VALUES ($1, $2, $3, $4, $5, $6)",
-    [first_name, last_name, date_of_birth, phone_number, date_joined, title],
-    (error) => {
-      if (error) {
-        console.error("Error executing query", error);
-        return res.status(500).send("Internal Server Error");
-      }
+
+  pool
+    .query(
+      "INSERT INTO members (first_name, last_name, date_of_birth, phone_number, date_joined, title ) VALUES ($1, $2, $3, $4, $5, $6)",
+      [first_name, last_name, date_of_birth, phone_number, date_joined, title]
+    )
+    .then(() => {
       console.log("New member has been added:", first_name, last_name);
       res.redirect("/members");
-    }
-  );
-});
-
-app.get("/members", (req, res, next) => {
-  pool.query("SELECT * FROM members", (error, result) => {
-    if (error) {
+    })
+    .catch((error) => {
       console.error("Error executing query", error);
-      return res.status(500).send("Internal Server Error");
-    }
-    res.render("members", { members: result.rows });
-  });
+      res.status(500).send("Internal Server Error");
+    });
 });
 
-app.get("/edit/:id", (req, res, next) => {
+app.get("/members", (req, res) => {
+  pool
+    .query("SELECT * FROM members")
+    .then((result) => {
+      res.render("members", { members: result.rows });
+    })
+    .catch((error) => {
+      console.error("Error executing query", error);
+      res.status(500).send("Internal Server Error");
+    });
+});
+
+app.get("/edit/:id", (req, res) => {
   const id = req.params.id;
-  pool.query("SELECT * FROM members WHERE id = $1", [id], (error, result) => {
-    if (error) {
+
+  pool
+    .query("SELECT * FROM members WHERE id = $1", [id])
+    .then((result) => {
+      res.render("edit", { member: result.rows[0] });
+    })
+    .catch((error) => {
       console.error("Error executing query", error);
-      return res.status(500).send("Internal Server Error");
-    }
-    res.render("edit", { member: result.rows[0] });
-  });
+      res.status(500).send("Internal Server Error");
+    });
 });
 
-app.post("/update/:id", (req, res, next) => {
+app.post("/update/:id", (req, res) => {
   const id = req.params.id;
   const {
     first_name,
@@ -86,38 +91,43 @@ app.post("/update/:id", (req, res, next) => {
     date_joined,
     title,
   } = req.body;
-  pool.query(
-    "UPDATE members SET first_name = $1, last_name = $2, date_of_birth = $3, phone_number = $4, date_joined = $5, title = $6 WHERE id = $7",
-    [
-      first_name,
-      last_name,
-      date_of_birth,
-      phone_number,
-      date_joined,
-      title,
-      id,
-    ],
-    (error) => {
-      if (error) {
-        console.error("Error executing query", error);
-        return res.status(500).send("Internal Server Error");
-      }
+
+  pool
+    .query(
+      "UPDATE members SET first_name = $1, last_name = $2, date_of_birth = $3, phone_number = $4, date_joined = $5, title = $6 WHERE id = $7",
+      [
+        first_name,
+        last_name,
+        date_of_birth,
+        phone_number,
+        date_joined,
+        title,
+        id,
+      ]
+    )
+    .then(() => {
       console.log("Member has been updated");
       res.redirect("/members");
-    }
-  );
+    })
+    .catch((error) => {
+      console.error("Error executing query", error);
+      res.status(500).send("Internal Server Error");
+    });
 });
 
-app.post("/delete/:id", (req, res, next) => {
+app.post("/delete/:id", (req, res) => {
   const id = req.params.id;
-  pool.query("DELETE FROM members WHERE id = $1", [id], (error) => {
-    if (error) {
+
+  pool
+    .query("DELETE FROM members WHERE id = $1", [id])
+    .then(() => {
+      console.log("Member has been deleted");
+      res.redirect("/");
+    })
+    .catch((error) => {
       console.error("Error executing query", error);
-      return res.status(500).send("Internal Server Error");
-    }
-    console.log("Member has been deleted");
-    res.redirect("/");
-  });
+      res.status(500).send("Internal Server Error");
+    });
 });
 
 const port = process.env.PORT || 3000;
